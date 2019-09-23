387d6aa310d152c7cb7dc1e8a19350e53fcac05e
Denis Fateyev <denis@fateyev.com>
2019-09-23 12:51:05 +0600

    change manifest version to 1.15.5

--- scripts/beforeinit.js
+++ scripts/beforeinit.js
@@ -45,7 +45,7 @@ for (var i = 0; i < quotas.length; i++){
     } 
 }
 var resp = {result:0};
-var url = "https://raw.githubusercontent.com/jelastic-jps/kubernetes/v1.15.4/configs/settings.yaml";
+var url = "https://raw.githubusercontent.com/jelastic-jps/kubernetes/v1.15.5/configs/settings.yaml";
 resp.settings = toNative(new org.yaml.snakeyaml.Yaml().load(new com.hivext.api.core.utils.Transport().get(url)));
 if (markup) {
   var f = resp.settings.fields;
