a5447da637a44d746dc62e70e8b0b3fd222d4b78
bianchi2 <ivantsoft@gmail.com>
2019-10-10 17:53:43 +0300

    Add Jaeger check

--- scripts/check-install.sh
+++ scripts/check-install.sh
@@ -9,6 +9,7 @@ Options:
  -m=,   --monitoring=        check monitoring tools, defaults to false
  -r=,   --remote-api=        check remote api availability, defaults to false
  -s=,   --storage=           check NFS storage, defaults to false
+ -j=,   --jaeger=            check Jaeger installation
  -app=, --sample-app         check either defualt Hello World app (cc) or a custom syntax [cmd], defaults to cc
  -h,    --help               show this help
 "
@@ -45,6 +46,10 @@ do
       STORAGE=$(echo "${key#*=}")
       shift
       ;;
+    -j=*| --jaeger=*)
+      JAEGER=$(echo "${key#*=}")
+      shift
+      ;;
     -h | --help)
       echo -e "$HELP"
       exit 1
@@ -68,7 +73,8 @@ or set a local DOMAIN variable:
 DOMAIN=nydomain.jelastic.com; $0 [options]"
     exit 1
 fi
-K8S_EVENTS_LOG_FILE="/var/log/k8s-events.log"
+K8S_LOG_DIR="/var/log/kubernetes"
+K8S_EVENTS_LOG_FILE="/var/log/kubernetes/k8s-events.log"
 METRICS_SERVER_NAME="metrics-server"
 CNI_PLUGIN_NAME="weave-net"
 DASHBOARD_DEPLOYMENT_NAME="kubernetes-dashboard"
@@ -87,6 +93,8 @@ INGRESS_CONTROLLER=${INGRESS_CONTROLLER:-${DEFAULT_INGRESS_CONTROLLER}}
 DEFAULT_REMOTE_API="false"
 REMOTE_API=${REMOTE_API:-${DEFAULT_REMOTE_API}}
 
+DEFAULT_JAEGER="false"
+JAEGER=${JAEGER:-${DEFAULT_JAEGER}}
 
 
 printInfo() {
@@ -134,8 +142,8 @@ checkWeaveStatus() {
         printInfo "Checking status on Node $NODENAME"
         if [ "$STATUS" != "Running" ]; then
           printError "Failed ${CNI_PLUGIN_NAME} pod ${PODNAME} on $NODENAME with status ${STATUS}"
-          kubectl logs ${PODNAME} -n kube-system > /var/log/${PODNAME}.log
-          printError "Check logs in /var/log/${PODNAME}.log"
+          kubectl logs ${PODNAME} -n kube-system > ${K8S_LOG_DIR}/${PODNAME}.log
+          printError "Check logs in ${K8S_LOG_DIR}/${PODNAME}.log"
           WITH_ERROR="true"
         else
           printInfo "${CNI_PLUGIN_NAME} pod ${PODNAME} on $NODENAME successfully started"
@@ -162,9 +170,9 @@ checkMetricsServer() {
       else
         STATUS=$(kubectl get pods/${METRICS_SERVER_POD} -n kube-system -o jsonpath="{.status.phase}" 2> /dev/null)
         printError "Metrics server is not running and currently in status ${STATUS}"
-        printError "${METRICS_SERVER_NAME} pod logs are available in /var/log/metrics-server.log"
+        printError "${METRICS_SERVER_NAME} pod logs are available in ${K8S_LOG_DIR}/${METRICS_SERVER_POD}.log"
         printError "Inspect K8s events in ${K8S_EVENTS_LOG_FILE}"
-        kubectl logs ${METRICS_SERVER_POD} -n kube-system > /var/log/metrics-server.log
+        kubectl logs ${METRICS_SERVER_POD} -n kube-system > ${K8S_LOG_DIR}/${METRICS_SERVER_POD}.log
         WITH_ERROR="true"
       fi
     else
@@ -191,9 +199,9 @@ checkDashboard() {
       else
         DASHBOARD_POD_STATUS=$(kubectl get pods/$KUBERNETES_DASHBOARD_POD -n kubernetes-dashboard -o jsonpath="{.status.phase}" 2> /dev/null)
         printError "Kubernetes Dashboard is not running. Current status is: ${DASHBOARD_POD_STATUS}"
-        printError "${DASHBOARD_DEPLOYMENT_NAME} pod logs are available in /var/log/kubernetes-dashboard.log"
+        printError "${DASHBOARD_DEPLOYMENT_NAME} pod logs are available in ${K8S_LOG_DIR}/kubernetes-dashboard.log"
         printError "Inspect K8s events in ${K8S_EVENTS_LOG_FILE} on a master node"
-        kubectl logs ${KUBERNETES_DASHBOARD_POD} -n ${DASHBOARD_DEPLOYMENT_NAME} > /var/log/kubernetes-dashboard.log
+        kubectl logs ${KUBERNETES_DASHBOARD_POD} -n ${DASHBOARD_DEPLOYMENT_NAME} > ${K8S_LOG_DIR}/kubernetes-dashboard.log
         WITH_ERROR="true"
       fi
     else
@@ -226,8 +234,8 @@ checkTraefikIngressController() {
     printInfo "Checking Traefik pod status on Node $NODENAME"
     if [ "$STATUS" != "Running" ]; then
       printError "Failed Traefik pod ${PODNAME} on $NODENAME with status: $STATUS"
-      kubectl logs ${PODNAME} -n kube-system > /var/log/${PODNAME}.log
-      printError "Check logs in /var/log/${PODNAME}.log"
+      kubectl logs ${PODNAME} -n kube-system > ${K8S_LOG_DIR}/${PODNAME}.log
+      printError "Check logs in ${K8S_LOG_DIR}/${PODNAME}.log"
       WITH_ERROR="true"
       INGRESS_STATUS="FAIL"
     else
@@ -254,9 +262,9 @@ checkNginxIngressController() {
         WITH_ERROR="true"
       else
         printError "${NGINX_DEPLOYMENT_NAME} is not running"
-        printError "${NGINX_DEPLOYMENT_NAME} pod logs are available in /var/log/${NGINX_POD}.log"
+        printError "${NGINX_DEPLOYMENT_NAME} pod logs are available in ${K8S_LOG_DIR}/${NGINX_POD}.log"
         printError "Inspect K8s events in ${K8S_EVENTS_LOG_FILE}"
-        kubectl logs ${NGINX_POD} -n ingress-nginx > /var/log/${NGINX_POD}.log
+        kubectl logs ${NGINX_POD} -n ingress-nginx > ${K8S_LOG_DIR}/${NGINX_POD}.log
         INGRESS_STATUS="FAIL"
         WITH_ERROR="true"
       fi
@@ -286,8 +294,8 @@ checkHaproxyIngressController() {
     HAPROXY_POD_STATUS=$(kubectl get pods -l=run=haproxy-ingress -n ingress-controller -o jsonpath='{.items[0].status.phase}' 2> /dev/null)
     if [ "$HAPROXY_POD_STATUS" != "Running" ]; then
       printError "HAProxy pod isn't in running state. Current status: $HAPROXY_POD_STATUS"
-      kubectl logs ${PODNAME} -n ingress-controller > /var/log/${PODNAME}.log
-      printError "Check logs in /var/log/${PODNAME}.log"
+      kubectl logs ${PODNAME} -n ingress-controller > ${K8S_LOG_DIR}/${PODNAME}.log
+      printError "Check logs in ${K8S_LOG_DIR}/${PODNAME}.log"
       INGRESS_STATUS="FAIL"
       WITH_ERROR="true"
     else
@@ -320,9 +328,11 @@ checkRemoteApi() {
   elif [ $OUT -ne 0 ]; then
     printError "Remote API $URL is unavailable. cURL exit code is $OUT"
     WITH_ERROR="true"
+    REMOTEAPI_STATUS="FAIL"
   elif [ $STATUSCODE -ne 200 ]; then
     printError "Remote API $URL is unavailable and returned ${STATUSCODE}"
     WITH_ERROR="true"
+    REMOTEAPI_STATUS="FAIL"
   else
     printInfo "Remote API $URL is available and returned ${STATUSCODE}"
     REMOTEAPI_STATUS="OK"
@@ -349,7 +359,7 @@ checkSampleApp() {
 fi
   printInfo "Checking ${APP}"
   if [ "${SAMPLE_APP}" == "cmd" ]; then
-    OPEN_LIBERTY_INGRESS=$(kubectl get ingress/open-liberty -n default --template '{{range .items}}{{.metadata.name}}{{"\n"}}{{end}}' 2> /dev/null)
+    kubectl get ingresses | grep open-liberty > /dev/null
     if [ $? -ne 0 ]; then
       printWarning "Default OpenLiberty ingress not found. Perhaps CMD command was modified"
       printWarning "Disregard this warning if you modified a custom cmd command"
@@ -372,14 +382,16 @@ checkNfsStorage() {
       printError "Failed to find NFS provisioner pods. NFS privioners may have failed or has not been deployed"
       printError "Check K8s events in ${K8S_EVENTS_LOG_FILE} on a master node"
       WITH_ERROR="true"
+      NFS_STORAGE_STATUS="FAIL"
       break
     fi
     STATUS=$(kubectl get pods -l=app=nfs-client-provisioner -n default -o jsonpath="{.items[$i].status.phase}" 2> /dev/null)
     if [ "$STATUS" != "Running" ]; then
       printError "Failed pod ${PODNAME} with status $STATUS"
-      kubectl logs ${PODNAME} -n default > /var/log/${PODNAME}.log
-      printError "Check logs in /var/log/${PODNAME}.log"
+      kubectl logs ${PODNAME} -n default > ${K8S_LOG_DIR}/${PODNAME}.log
+      printError "Check logs in ${K8S_LOG_DIR}/${PODNAME}.log"
       WITH_ERROR="true"
+      NFS_STORAGE_STATUS="FAIL"
     else
       printInfo "Provisioner pod ${PODNAME} is running"
       NFS_STORAGE_STATUS="OK"
@@ -408,6 +420,7 @@ checkMonitoring() {
     if [ $? -ne 0 ]; then
       printError "deployment ${DEPLOYMENTS[i]} not found. Check installation logs (CS) and K8s events in ${K8S_EVENTS_LOG_FILE} on a master node"
       MONIT_ERROR="true"
+      MONITORING_STATUS="FAIL"
     else
       if [ "${readyReplicas}" -lt 1 ]; then
         printInfo "Deployment ${DEPLOYMENTS[i]} isn't scaled to 1. Checking pods logs..."
@@ -423,21 +436,22 @@ checkMonitoring() {
         if [ -z $MONIT_POD ]; then
           printError "Failed to find ${MONIT_POD} pod"
           MONIT_ERROR="true"
-          WITH_ERROR="true"
+          MONITORING_STATUS="FAIL"
         else
           printError "${MONIT_POD} is not running"
-          printError "${MONIT_POD} pod logs are available in /var/log/${MONIT_POD}.log"
+          printError "${MONIT_POD} pod logs are available in ${K8S_LOG_DIR}/${MONIT_POD}.log"
           printError "It is recommended to inspect K8s events in ${K8S_EVENTS_LOG_FILE}"
           if [ ${DEPLOYMENTS[i]} == "monitoring-prometheus-server" ]; then
-            kubectl logs ${MONIT_POD} -c prometheus-server-configmap-reload -n kubernetes-monitoring > /var/log/${MONIT_POD}.log
-            kubectl logs ${MONIT_POD} -c prometheus-server -n kubernetes-monitoring >> /var/log/${MONIT_POD}.log
+            kubectl logs ${MONIT_POD} -c prometheus-server-configmap-reload -n kubernetes-monitoring > ${K8S_LOG_DIR}/${MONIT_POD}.log
+            kubectl logs ${MONIT_POD} -c prometheus-server -n kubernetes-monitoring >> ${K8S_LOG_DIR}/${MONIT_POD}.log
           elif [ ${DEPLOYMENTS[i]} == "monitoring-prometheus-alertmanager" ]; then
-            kubectl logs ${MONIT_POD} -c prometheus-alertmanager-configmap-reload -n kubernetes-monitoring > /var/log/${MONIT_POD}.log
-            kubectl logs ${MONIT_POD} -c prometheus-alertmanager -n kubernetes-monitoring >> /var/log/${MONIT_POD}.log
+            kubectl logs ${MONIT_POD} -c prometheus-alertmanager-configmap-reload -n kubernetes-monitoring > ${K8S_LOG_DIR}/${MONIT_POD}.log
+            kubectl logs ${MONIT_POD} -c prometheus-alertmanager -n kubernetes-monitoring >> ${K8S_LOG_DIR}/${MONIT_POD}.log
           else
-          kubectl logs ${MONIT_POD} -n kubernetes-monitoring > /var/log/${MONIT_POD}.log
+          kubectl logs ${MONIT_POD} -n kubernetes-monitoring > ${K8S_LOG_DIR}/${MONIT_POD}.log
         fi
           MONIT_ERROR="true"
+          MONITORING_STATUS="FAIL"
         fi
       fi
     fi
@@ -448,6 +462,7 @@ checkMonitoring() {
   if [ $STATUSCODE -ne 200 ]; then
     printError "$URL is unavailable. cURL exit code is $STATUSCODE"
     MONIT_ERROR="true"
+    MONITORING_STATUS="FAIL"
   else
     printInfo "$URL is available and returned ${STATUSCODE}"
     MONITORING_STATUS="OK"
@@ -465,19 +480,19 @@ checkNodeProblemDetector(){
   printInfo "Checking Node problem detector deployment"
   POD_STATUS=$(kubectl get pods -l=app=node-problem-detector -n default -o jsonpath="{.items[0]}" 2> /dev/null)
   if [ $? -ne 0 ]; then
-    kubectl get ds/node-problem-detector -o yaml &>> /var/log/node-problem-detector.yaml
+    kubectl get ds/node-problem-detector -o yaml &>> ${K8S_LOG_DIR}/node-problem-detector.yaml
     if [ $? -ne 0 ]; then
       printError "Node-problem detector daemon set not found"
-      printError "Check available DaemonSets in /var/log/daemonsets-default-ns.log"
-      writeLog "kubectl get ds" "/var/log/daemonsets.log"
+      printError "Check available DaemonSets in ${K8S_LOG_DIR}/daemonsets-default-ns.log"
+      writeLog "kubectl get ds" "${K8S_LOG_DIR}/daemonsets.log"
       WITH_ERROR="true"
     else
-      DEFAULT_NS_PODS_LOG_FILE="/var/log/pods-default-ns.log"
-      NODE_PROBLEM_DETECTOR_PODS_LOG_FILE="/var/log/node-problem-detector-pods.log"
+      DEFAULT_NS_PODS_LOG_FILE="${K8S_LOG_DIR}/pods-default-ns.log"
+      NODE_PROBLEM_DETECTOR_PODS_LOG_FILE="${K8S_LOG_DIR}/node-problem-detector-pods.log"
       writeLog "kubectl get pods -n default" ${DEFAULT_NS_PODS_LOG_FILE}
       writeLog "kubectl get pods -l=app=node-problem-detector -n default" ${NODE_PROBLEM_DETECTOR_PODS_LOG_FILE}
       printError "No node-problem-detector pods found. Daemon set has been created but no pods have been scheduled yet"
-      printError "Check ${NODE_PROBLEM_DETECTOR_PODS_LOG_FILE}, ${DEFAULT_NS_PODS_LOG_FILE}, /var/log/node-problem-detector.yaml, and K8s events in ${K8S_EVENTS_LOG_FILE} on a master node"
+      printError "Check ${NODE_PROBLEM_DETECTOR_PODS_LOG_FILE}, ${DEFAULT_NS_PODS_LOG_FILE}, ${K8S_LOG_DIR}/node-problem-detector.yaml, and K8s events in ${K8S_EVENTS_LOG_FILE} on a master node"
       WITH_ERROR="true"
     fi
   else
@@ -497,8 +512,8 @@ checkNodeProblemDetector(){
       printInfo "Checking node-problem-detector pod status on Node $NODENAME"
       if [ "$STATUS" != "Running" ]; then
         printError "Failed node-problem-detector pod ${PODNAME} on $NODENAME with status $STATUS"
-        writeLog "kubectl logs ${PODNAME} -n default" "/var/log/${PODNAME}.log"
-        printError "Check logs in /var/log/${PODNAME}.log"
+        writeLog "kubectl logs ${PODNAME} -n default" "${K8S_LOG_DIR}/${PODNAME}.log"
+        printError "Check logs in ${K8S_LOG_DIR}/${PODNAME}.log"
         WITH_ERROR="true"
       else
         printInfo "Node-problem-detector pod ${PODNAME} on $NODENAME successfully started"
@@ -508,6 +523,41 @@ checkNodeProblemDetector(){
  fi
 }
 
+checkJaeger() {
+  printInfo "Checking Jaeger deployment..."
+  kubectl get jaeger/jaeger -n observability > /dev/null 2>&1
+  if [ $? -ne 0 ]; then
+    printError "Jaeger CR not found"
+    WITH_ERROR="true"
+    JAEGER_STATUS="FAIL"
+  else
+    READY_REPLICAS=$(kubectl get deployment/jaeger -o=jsonpath='{.status.readyReplicas}' -n observability 2> /dev/null)
+    if [ $? -ne 0 ]; then
+      printError "Jaeger deployment not found"
+      WITH_ERROR="true"
+      JAEGER_STATUS="FAIL"
+    else
+      if [ -z "$READY_REPLICAS" ]; then
+        sleep 10
+        READY_REPLICAS=$(kubectl get deployment/jaeger -o=jsonpath='{.status.readyReplicas}' -n observability 2> /dev/null)
+      fi
+      if [ "${READY_REPLICAS}" -lt 1 ]; then
+        printInfo "Jaeger deployment isn't scaled to 1. Checking pods logs..."
+        JAEGER_POD=$(kubectl get pods -l=app.kubernetes.io/managed-by=jaeger-operator -n observability -o jsonpath="{.items[0].metadata.name}")
+        kubectl logs ${JAEGER_POD} -n observability > ${K8S_LOG_DIR}/${JAEGER_POD}.log
+        printError "Check logs in ${K8S_LOG_DIR}/${JAEGER_POD}.log"
+        WITH_ERROR="true"
+        JAEGER_STATUS="FAIL"
+      else
+        printInfo "Jaeger is running"
+        JAEGER_STATUS="OK"
+      fi
+    fi
+
+  fi
+
+}
+
 printEvents() {
   printInfo "Saving events to ${K8S_EVENTS_LOG_FILE}"
   writeLog "kubectl get events --all-namespaces" ${K8S_EVENTS_LOG_FILE}
@@ -515,14 +565,7 @@ printEvents() {
 
 generateReport() {
 
-  if [ "${REMOTE_API}" != "true" ] || [ "${MONITORING}" != "true" ] || [ "${STORAGE}" != "true" ]; then
-    CURRENT_STATUS="Not Enabled"
-  else
-    MONITORING_STATUS=${MONITORING_STATUS:-"FAIL"}
-    REMOTEAPI_STATUS=${REMOTEAPI_STATUS:-"FAIL"}
-    NFS_STORAGE_STATUS=${NFS_STORAGE_STATUS:-"FAIL"}
-  fi
-  echo -e "
+echo -e "
 Cluster Health Check Report
 
 [Weave CNI Plugin]      : ${WEAVE_STATUS:-"FAIL"}
@@ -530,9 +573,10 @@ Cluster Health Check Report
 [Metrics Server]        : ${METRICS_STATUS:-"FAIL"}
 [Kubernetes Dashboard]  : ${DASHBOARD_STATUS:-"FAIL"}
 [Node Problem Detector] : ${NODE_PROBLEM_DETECTOR_STATUS:-"FAIL"}
-[Monitoring Tools]      : ${MONITORING_STATUS:-${CURRENT_STATUS}}
-[Remote API]            : ${REMOTEAPI_STATUS:-${CURRENT_STATUS}}
-[NFS Storage]           : ${NFS_STORAGE_STATUS:-${CURRENT_STATUS}}
+[Monitoring Tools]      : ${MONITORING_STATUS:-"Not Enabled"}
+[Jaeger Tracing Tools]  : ${JAEGER_STATUS:-"Not Enabled"}
+[Remote API]            : ${REMOTEAPI_STATUS:-"Not Enabled"}
+[NFS Storage]           : ${NFS_STORAGE_STATUS:-"Not Enabled"}
 [Sample App]            : ${APP_STATUS:-"FAIL"}
   "
 
@@ -554,6 +598,9 @@ runCheck() {
   fi
   checkNodeProblemDetector
   checkSampleApp
+  if [ "${JAEGER}" == "true" ]; then
+    checkJaeger
+  fi
   printEvents
   generateReport
 }
