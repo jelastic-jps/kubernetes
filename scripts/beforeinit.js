236aa97f9c6be522820f41bb699a323e20e8fee2
Denis Fateyev <denis@fateyev.com>
2019-11-15 18:14:28 +0600

    k8s-test: synced to v1.15.6

--- scripts/beforeinit.js
+++ scripts/beforeinit.js
@@ -2,7 +2,7 @@
 var perEnv = "environment.maxnodescount",
     maxEnvs = "environment.maxcount",
     perNodeGroup = "environment.maxsamenodescount";
-var envsCount = jelastic.env.control.GetEnvs({lazy: true}).infos.length, 
+var envsCount = jelastic.env.control.GetEnvs({lazy: true}).infos.length,
     nodesPerProdEnv = 8,
     nodesPerProdEnvWOStorage = 7,
     nodesPerDevEnv = 3,
@@ -18,34 +18,34 @@ for (var i = 0; i < quotas.length; i++){
         err(q, "already used", envsCount, true);
         markup = "Maximum allowed environments: " + markup;
         prod = dev = storage = false; break;
-    } 
+    }
     if (n == perEnv && nodesPerProdEnv > q.value){
         if (!markup) err(q, "required", nodesPerProdEnv);
-        prod = false; 
-        if (nodesPerProdEnvWOStorage <= q.value) { 
+        prod = false;
+        if (nodesPerProdEnvWOStorage <= q.value) {
           prod = true;
           storage = false;
         }
-    }  
+    }
     if (n == perEnv && nodesPerDevEnv > q.value){
         err(q, "required", nodesPerDevEnv, true);
         dev = false;
-        if (nodesPerDevEnvWOStorage <= q.value) { 
+        if (nodesPerDevEnvWOStorage <= q.value) {
           dev = true;
           storage = false;
         }
-    }  
+    }
     if (n == perNodeGroup && nodesPerMasterNG > q.value){
         if (!markup) err(q, "required", nodesPerMasterNG);
         prod = false;
-    }  
+    }
     if (n == perNodeGroup && nodesPerWorkerNG > q.value){
         err(q, "required", nodesPerWorkerNG);
         dev = false;
-    } 
+    }
 }
 var resp = {result:0};
-var url = "https://raw.githubusercontent.com/jelastic-jps/kubernetes/v1.15.4/configs/settings.yaml";
+var url = "https://raw.githubusercontent.com/jelastic-jps/kubernetes/k8s-test/configs/settings.yaml";
 resp.settings = toNative(new org.yaml.snakeyaml.Yaml().load(new com.hivext.api.core.utils.Transport().get(url)));
 if (markup) {
   var f = resp.settings.fields;
@@ -55,7 +55,7 @@ if (markup) {
       "height": 30,
       "hideLabel": true,
       "markup": (!prod && dev  ? "Production topology is not available. " : "") + markup + "Please upgrade your account."
-  }); 
+  });
   if (!prod && !dev){
     f.push({
         "type": "compositefield",
@@ -70,9 +70,9 @@ if (markup) {
     });
   } else {
     if (!prod) delete f[2].values["1-prod"];
-    if (!storage) f.splice(3, 1);    
+    if (!storage) f.splice(3, 1);
   }
-}  
+}
 return resp;
 
 function err(e, text, cur, override){
