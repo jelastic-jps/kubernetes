de96f6ef63373e536fd52153962632961ce867c0
Denis Fateyev <denis@fateyev.com>
2020-03-02 16:02:43 +0600

    K8S-258 offload optimization

---
+++ scripts/helm-components.sh
@@ -0,0 +1,89 @@
+#!/bin/bash
+# set -x
+
+HELP="Usage:
+	$0 --base-url=<base64-encoded-url> --nfs-provisioner=(true|false) --nfs-server=<ip-address> --metallb=(true|false) --problem-detector=(true|false)
+Options:
+	--base-url            manifest baseUrl
+	--nfs-provisioner=    install nfs-client-provisioner
+	--nfs-server=         nfs-client-provisioner NFS server address
+	--metallb=            install metallb-controller
+	--problem-detector=   install node-problem-detector
+	-h, --help    show this help
+"
+if [[ $# -eq 0 ]] ; then
+	echo -e "${HELP}"
+	exit 2
+fi
+
+for key in "$@"; do
+	case $key in
+	--base-url=*)
+		BASE_URL="${key#*=}"
+		shift
+		;;
+	--nfs-provisioner=*)
+		NFS_INSTALL="${key#*=}"
+		shift
+		;;
+	--nfs-server=*)
+		NFS_SERVER="${key#*=}"
+		shift
+		;;
+	--metallb=*)
+		METALLB="${key#*=}"
+		shift
+		;;
+	--problem-detector=*)
+		PROBLEM_DETECT="${key#*=}"
+		shift
+		;;
+	-h | --help)
+		echo -e "${HELP}"
+		exit 1
+		;;
+	*)
+		echo "Unknown argument passed: '$key'"
+		echo -e "${HELP}"
+		exit 1
+		;;
+	esac
+done
+
+if [ -z "${BASE_URL}" ]; then
+	echo -e "Missing mandatory argument --base-url=<base64-encoded-url>"
+	exit 1
+fi
+
+( ( echo "$(date): Helm components started ---";
+
+	BASE_URL="$(echo ${BASE_URL} | base64 --decode)"
+
+	if [ "x${NFS_INSTALL}" = "xtrue" ] && [ -n "${NFS_SERVER}" ]; then
+		echo "$(date): installing nfs-client-provisioner"
+		helm install stable/nfs-client-provisioner --name nfs-client-provisioner --set nfs.server=${NFS_SERVER} --set nfs.path=/data --set nfs.mountOptions='{soft,proto=tcp}' --set replicaCount=3 --set storageClass.defaultClass=true --set storageClass.allowVolumeExpansion=true --set storageClass.name=jelastic-dynamic-volume
+	else
+		echo "$(date): nfs-client-provisioner installation skipped"
+	fi
+
+	if [ "x${METALLB}" = "xtrue" ]; then
+		echo "$(date): installing metallb-controller"
+		kubectl apply -f https://raw.githubusercontent.com/google/metallb/v0.8.3/manifests/metallb.yaml;
+		kubectl apply -f "${BASE_URL}/addons/metallb-config.yaml";
+	else 
+		echo "$(date): metallb-controller installation skipped"
+	fi
+
+	if [ "x${PROBLEM_DETECT}" = "xtrue" ]; then
+		echo "$(date): installing node-problem-detector"
+		helm install stable/node-problem-detector --name node-problem-detector --set image.tag=v0.7.0;
+	else
+		echo "$(date): node-problem-detector installation skipped"
+	fi
+
+	echo "$(date): Helm components finished ---";
+) &>>/var/log/kubernetes/k8s-helm-components.log & )&
+
+echo "${HOSTNAME} Helm components spawned"
+
+exit 0
---
+++ scripts/install-components.sh
@@ -0,0 +1,115 @@
+#!/bin/bash
+# set -x
+
+# core
+K8S="1.16.7"
+PAUSE="3.1"
+ETCD="3.3.15-0"
+COREDNS="1.6.2"
+WEAVE="2.5.2"
+
+# components
+K9S="0.17.2"
+POPEYE="0.7.0"
+STERN="1.11.0"
+KUBECTX="0.8.0"
+
+HELP="Usage:
+	$0 --type=(master|worker) --repo=<dockerhub-repo>
+Options:
+	--type=       instance type (values: master, worker)
+	--repo=       repository used (arbitrary)
+	-h, --help    show this help
+"
+if [[ $# -eq 0 ]] ; then
+	echo -e "${HELP}"
+	exit 2
+fi
+
+for key in "$@"; do
+	case $key in
+	--type=*)
+		COMPTYPE="${key#*=}"
+		shift
+		;;
+	--repo=*)
+		REPOSITORY="${key#*=}"
+		shift
+		;;
+	-h | --help)
+		echo -e "${HELP}"
+		exit 1
+		;;
+	*)
+		echo "Unknown argument passed: '$key'"
+		echo -e "${HELP}"
+		exit 1
+		;;
+	esac
+done
+
+if [ -z "${COMPTYPE}" ]; then
+	echo -e "Missing mandatory argument --type=(master|worker)"
+	exit 1
+fi
+if [ "x${COMPTYPE}" != "xmaster" ] && [ "x${COMPTYPE}" != "xworker" ]; then
+	echo -e "Invalid argument value --type=${COMPTYPE}"
+	exit 1
+fi
+if [ -z "${REPOSITORY}" ]; then
+	echo -e "Missing mandatory argument --repo=<dockerhub-repo>"
+	exit 1
+fi
+
+# common
+docker pull ${REPOSITORY}/kube-proxy:v${K8S}
+[ -n "${PAUSE}" ] && docker pull ${REPOSITORY}/pause:${PAUSE}
+
+# kubernetes
+[ "x${COMPTYPE}" = "xmaster" ] && {
+	docker pull ${REPOSITORY}/kube-apiserver:v${K8S};
+	docker pull ${REPOSITORY}/kube-controller-manager:v${K8S};
+	docker pull ${REPOSITORY}/kube-scheduler:v${K8S};
+	[ -n "${ETCD}" ] && docker pull ${REPOSITORY}/etcd:${ETCD};
+	[ -n "${COREDNS}" ] && docker pull ${REPOSITORY}/coredns:${COREDNS};
+}
+
+# weave
+[ -n "${WEAVE}" ] && {
+	docker pull jelastic/weave-npc:${WEAVE};
+	docker pull jelastic/weave-kube:${WEAVE};
+}
+
+# additional
+[ "x${COMPTYPE}" = "xmaster" ] && {
+	[ -n "${WEAVE}" ] && {
+		docker pull weaveworks/weaveexec:${WEAVE};
+		wget -nv "https://github.com/weaveworks/weave/releases/download/v${WEAVE}/weave" -O /usr/bin/weave;
+		chmod +x /usr/bin/weave;
+	};
+
+	# scripts
+	BASE_URL="https://raw.githubusercontent.com/jelastic-jps/kubernetes/v${K8S}";
+
+	wget -nv "${BASE_URL}/scripts/helm-slave.sh" -O /usr/local/sbin/helm-slave.sh;
+	chmod +x /usr/local/sbin/helm-slave.sh;
+	wget -nv "${BASE_URL}/scripts/helm-components.sh" -O /usr/local/sbin/helm-components.sh;
+	chmod +x /usr/local/sbin/helm-components.sh;
+	wget -nv "${BASE_URL}/scripts/check-install.sh" -O /usr/local/sbin/check-install.sh;
+	chmod +x /usr/local/sbin/check-install.sh;
+
+	# utilities
+	[ -n "${K9S}" ] && { wget -O- "https://github.com/derailed/k9s/releases/download/v${K9S}/k9s_Linux_x86_64.tar.gz" | tar xz -C /usr/bin k9s; };
+	[ -n "${POPEYE}" ] && { wget -O- "https://github.com/derailed/popeye/releases/download/v${POPEYE}/popeye_${POPEYE}_Linux_x86_64.tar.gz" | tar xz -C /usr/bin popeye; };
+	[ -n "${STERN}" ] && {
+		wget -nv "https://github.com/wercker/stern/releases/download/${STERN}/stern_linux_amd64" -O /usr/bin/stern;
+		chmod +x /usr/bin/stern;
+		/usr/bin/stern --completion=bash > /etc/bash_completion.d/stern.bash;
+	};
+	[ -n "${KUBECTX}" ] && {
+		wget -O- "https://github.com/ahmetb/kubectx/archive/v${KUBECTX}.tar.gz" | tar xz --strip-components=1 -C /usr/bin kubectx-${KUBECTX}/kubectx kubectx-${KUBECTX}/kubens;
+		wget -O- "https://github.com/ahmetb/kubectx/archive/v${KUBECTX}.tar.gz" | tar xz --strip-components=2 -C /etc/bash_completion.d kubectx-${KUBECTX}/completion/kubens.bash kubectx-${KUBECTX}/completion/kubectx.bash;
+	};
+}
+
+exit 0
