be070afeea1a0166be823852e9badecafddcd232
Slava Katiukha <sk@jelastic.com>
2021-05-05 09:40:09 +0300

    K8S-491 [K8s] Arrange elements more compactly/optimally in k8s dialog

--- scripts/beforeinit.js
+++ scripts/beforeinit.js
@@ -2,9 +2,9 @@
 var perEnv = "environment.maxnodescount",
     maxEnvs = "environment.maxcount",
     perNodeGroup = "environment.maxsamenodescount";
-    maxCloudletsPerRec = "environment.maxcloudletsperrec";
-    diskIOPSlimit = "disk.iopslimit";
-var envsCount = jelastic.env.control.GetEnvs({lazy: true}).infos.length,
+maxCloudletsPerRec = "environment.maxcloudletsperrec";
+diskIOPSlimit = "disk.iopslimit",
+    envsCount = jelastic.env.control.GetEnvs({lazy: true}).infos.length,
     nodesPerProdEnv = 10,
     nodesPerProdEnvWOStorage = 7,
     nodesPerDevEnv = 3,
@@ -17,7 +17,7 @@ var envsCount = jelastic.env.control.GetEnvs({lazy: true}).infos.length,
 
 var quotas = jelastic.billing.account.GetQuotas(perEnv + ";"+maxEnvs+";" + perNodeGroup + ";" + maxCloudletsPerRec + ";" + diskIOPSlimit).array;
 var group = jelastic.billing.account.GetAccount(appid, session);
-for (var i = 0; i < quotas.length; i++){
+for (var i = 0, l = quotas.length; i < l; i++) {
     var q = quotas[i], n = toNative(q.quota.name);
 
     if (n == maxEnvs && envsCount >= q.value){
@@ -29,8 +29,8 @@ for (var i = 0; i < quotas.length; i++){
         err(q, "required", maxCloudlets, true);
         prod = dev = false;
     }
-    
-     if (n == diskIOPSlimit && iopsLimit > q.value){
+
+    if (n == diskIOPSlimit && iopsLimit > q.value){
         err(q, "required", iopsLimit, true);
         prod = dev = false;
     }
@@ -64,31 +64,36 @@ var url = "https://raw.githubusercontent.com/jelastic-jps/kubernetes/bootstrap/c
 resp.settings = toNative(new org.yaml.snakeyaml.Yaml().load(new com.hivext.api.core.utils.Transport().get(url)));
 var f = resp.settings.fields;
 
-if (!prod && dev){
-    f[2].values[1].disabled = true;
-    f[3].hidden = false;
-    f[3].markup =  "Production topology is not available. " + markup + "Please upgrade your account.";
-    f[3].height =  50;
-    if (!devStorage){
-        f[6].disabled = true;
-        f[6].value = false;
+if (!prod && dev) {
+    //f[2].values[1].disabled = true;
+    f[1].items[0].disabled = true;
+    f[2].hidden = false;
+    f[2].markup =  "Production topology is not available. " + markup + "Please upgrade your account.";
+    f[2].height =  50;
+    if (!devStorage) {
+        f[4].disabled = true;
+        f[4].value = false;
     }
 }
 
 if (prod && !prodStorage){
-    f[6].disabled = true;
-    f[6].value = false;
+    f[4].disabled = true;
+    f[4].value = false;
 }
 
-if (!prod && !dev || group.groupType == 'trial'){
-    for (var i = 0; i < f.length; i++) f[i].disabled = true;
-    f[3].hidden = false;
-    f[3].disabled = false;
-    f[3].markup =  "Production and Development topologies are not available. " + markup + "Please upgrade your account.";
+if (!prod && !dev || group.groupType == 'trial') {
+    for (var i = 0, n = f.length; i < n; i++)
+        if (f[i].type == "compositefield") {
+            for (var j = 0, l = f[i].items.length; j < l; j++)  f[i].items[j].disabled = true;
+        } else f[i].disabled = true;
+
+    f[2].hidden = false;
+    f[2].disabled = false;
+    f[2].markup =  "Production and Development topologies are not available. " + markup + "Please upgrade your account.";
     if (group.groupType == 'trial')
-        f[3].markup = "Production and Development topologies are not available for " + group.groupType + " account. Please upgrade your account.";
-    f[3].height =  60;
-    f[6].value = false;
+        f[2].markup = "Production and Development topologies are not available for " + group.groupType + " account. Please upgrade your account.";
+    f[2].height =  60;
+    f[4].value = false;
 
     f.push({
         "type": "compositefield",
@@ -106,6 +111,6 @@ if (!prod && !dev || group.groupType == 'trial'){
 return resp;
 
 function err(e, text, cur, override){
-  var m = (e.quota.description || e.quota.name) + " - " + e.value + ", " + text + " - " + cur + ". ";
-  if (override) markup = m; else markup += m;
+    var m = (e.quota.description || e.quota.name) + " - " + e.value + ", " + text + " - " + cur + ". ";
+    if (override) markup = m; else markup += m;
 }
