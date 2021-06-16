4145ab44010f018d0a9a6746444b82cbe712def2
Denis Fateyev <denis@fateyev.com>
2021-06-17 00:52:29 +0600

    K8S-587 fix gitlab action

--- addons/gitlab/gitlab-integration-remove.sh
+++ addons/gitlab/gitlab-integration-remove.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+if [ ! -f "/var/lib/kubelet/kubeadm-flags.env" ]; then
+
+	echo -e "Kubernetes worker configuration is invalid"
+	exit 1
+fi
+
 if [ ! -f "/var/lib/worker/gitlab-integration.conf" ]; then
 
 	echo "$(date): Gitlab integration configuration is missing"
@@ -8,7 +14,15 @@ fi
 
 source /var/lib/worker/gitlab-integration.conf
 
-rm -rf "/etc/docker/certs.d/${GITLAB_REGISTRY}" &>/dev/null
+if grep -q 'containerd' /var/lib/kubelet/kubeadm-flags.env; then
+
+	rm -rf /etc/pki/ca-trust/source/anchors/gitlab-registry.crt &>/dev/null
+	/bin/update-ca-trust
+	service containerd restart
+
+else
+	rm -rf "/etc/docker/certs.d/${GITLAB_REGISTRY}" &>/dev/null
+fi
 
 echo "$(date): Gitlab integration configuration removed"
 
