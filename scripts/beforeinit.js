6a554b21ca5566f79a1dae710b31b9ef2ad4cd4a
Denis Fateyev <denis@fateyev.com>
2021-06-19 01:20:48 +0600

    K8S-592 bump version to 1.19.12

--- scripts/beforeinit.js
+++ scripts/beforeinit.js
@@ -60,7 +60,7 @@ for (var i = 0; i < quotas.length; i++){
     }
 }
 var resp = {result:0};
-var url = "https://raw.githubusercontent.com/jelastic-jps/kubernetes/v1.19.10/configs/settings.yaml";
+var url = "https://raw.githubusercontent.com/jelastic-jps/kubernetes/v1.19.12/configs/settings.yaml";
 resp.settings = toNative(new org.yaml.snakeyaml.Yaml().load(new com.hivext.api.core.utils.Transport().get(url)));
 var f = resp.settings.fields;
 
