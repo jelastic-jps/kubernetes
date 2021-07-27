5bef843dca124d9817480772d2a6fb07d840ed1f
Denis Fateyev <denis@fateyev.com>
2021-07-28 00:41:12 +0600

    K8S-586 bump component version

--- scripts/cplane-postconfig.sh
+++ scripts/cplane-postconfig.sh
@@ -2,17 +2,17 @@
 # set -x
 
 # components
-K9S="0.24.10"
-POPEYE="0.9.2"
+K9S="0.24.14"
+POPEYE="0.9.7"
 STERN="1.11.0"
-KUBECTX="0.9.3"
+KUBECTX="0.9.4"
 
 ( ( echo "$(date): --- cplane postconfig started";
 
 	# utilities
 	echo "$(date): retrieving k8s utilities";
-	[ -n "${K9S}" ] && { wget -O- "https://github.com/derailed/k9s/releases/download/v${K9S}/k9s_v${K9S}_Linux_x86_64.tar.gz" | tar xz -C /usr/bin k9s; };
-	[ -n "${POPEYE}" ] && { wget -O- "https://github.com/derailed/popeye/releases/download/v${POPEYE}/popeye_Linux_x86_64.tar.gz" | tar xz -C /usr/bin popeye; };
+	[ -n "${K9S}" ] && { wget -nv -O- "https://github.com/derailed/k9s/releases/download/v${K9S}/k9s_Linux_x86_64.tar.gz" | tar xz -C /usr/bin k9s; };
+	[ -n "${POPEYE}" ] && { wget -nv -O- "https://github.com/derailed/popeye/releases/download/v${POPEYE}/popeye_Linux_x86_64.tar.gz" | tar xz -C /usr/bin popeye; };
 	[ -n "${STERN}" ] && {
 		wget -nv "https://github.com/wercker/stern/releases/download/${STERN}/stern_linux_amd64" -O /usr/bin/stern;
 		chmod +x /usr/bin/stern;
@@ -20,10 +20,10 @@ KUBECTX="0.9.3"
 	};
 	[ -n "${KUBECTX}" ] && {
 		for KUBECTX_COMP in "kubens" "kubectx"; do
-			wget -O- "https://github.com/ahmetb/kubectx/releases/download/v${KUBECTX}/${KUBECTX_COMP}_v${KUBECTX}_linux_x86_64.tar.gz" | tar xz -C /usr/bin ${KUBECTX_COMP};
+			wget -nv -O- "https://github.com/ahmetb/kubectx/releases/download/v${KUBECTX}/${KUBECTX_COMP}_v${KUBECTX}_linux_x86_64.tar.gz" | tar xz -C /usr/bin ${KUBECTX_COMP};
 		done;
 
-		wget -O- "https://github.com/ahmetb/kubectx/archive/v${KUBECTX}.tar.gz" | tar xz --strip-components=2 -C /etc/bash_completion.d kubectx-${KUBECTX}/completion/kubens.bash kubectx-${KUBECTX}/completion/kubectx.bash;
+		wget -nv -O- "https://github.com/ahmetb/kubectx/archive/v${KUBECTX}.tar.gz" | tar xz --strip-components=2 -C /etc/bash_completion.d kubectx-${KUBECTX}/completion/kubens.bash kubectx-${KUBECTX}/completion/kubectx.bash;
 	};
 
 	# validate cplane configuration
