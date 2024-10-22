ca3b34a246cd2c53d47774a45833082dafd78d65
Dmitry Tsurko <dmytro.tsurko@virtuozzo.com>
2024-10-22 16:00:34 +0300

    K8S-810 Update K8s to new 1.30 version

--- scripts/beforeinit.js
+++ scripts/beforeinit.js
@@ -75,7 +75,7 @@ for (var i = 0; i < quotas.length; i++){
     }
 }
 var resp = {result:0};
-var url = "https://raw.githubusercontent.com/dimkadt/kubernetes/v1.29.9/configs/settings.yaml";
+var url = "https://raw.githubusercontent.com/jelastic-jps/kubernetes/v1.30.5/configs/settings.yaml";
 resp.settings = toNative(new org.yaml.snakeyaml.Yaml().load(new com.hivext.api.core.utils.Transport().get(url)));
 var f = resp.settings.fields;
 
