761bdfd6c7eb829f4a55df794e8da0dbd386db03
Dmytro Tsurko <22764205+dimkadt@users.noreply.github.com>
2024-10-25 14:51:34 +0300

    K8S-809 Update K8s to new 1.29 version (#60)
    * K8S-809 Update K8s to new 1.29 version
    
    * K8S-809 Update K8s to new 1.29 version - fix for C7
    
    ---------
    
    Co-authored-by: DmytroZubelevych <31444413+DmytroZubelevych@users.noreply.github.com>

--- scripts/beforeinit.js
+++ scripts/beforeinit.js
@@ -79,6 +79,13 @@ var url = "https://raw.githubusercontent.com/jelastic-jps/kubernetes/main/config
 resp.settings = toNative(new org.yaml.snakeyaml.Yaml().load(new com.hivext.api.core.utils.Transport().get(url)));
 var f = resp.settings.fields;
 
+let latestDefaultVersion = "v1.29.9";
+
+if (${fn.compareEngine(8.3)} == 1) {
+    f[0].items[0].default = latestDefaultVersion;
+    f[0].items[0].values.push({ caption: latestDefaultVersion, value: latestDefaultVersion });
+}
+
 if (!prod && dev) {
     //f[2].values[1].disabled = true;
     f[1].items[0].disabled = true;
